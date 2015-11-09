7b5a684800e0a742a9a6760d9416a230859feff5
student1 <student1@jh-mbair-158-12.d214.org>
2015-11-09 09:58:06 -0600

    added background colors

---
+++ ImageViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ImageViewController.swift
+//  Intergalactic Travel
+//
+//  Created by student1 on 11/6/15.
+//  Copyright © 2015 JohnHerseyHighSchool. All rights reserved.
+//
+
+import UIKit
+
+class ImageViewController: UIViewController {
+
+    var isBlueStar:Bool?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        if isBlueStar!
+        {
+            self.view.backgroundColor = UIColor.blueColor()
+        }
+        else
+        {
+            self.view.backgroundColor = UIColor.redColor()
+        }
+    
+    }
+
+        
+
+}
